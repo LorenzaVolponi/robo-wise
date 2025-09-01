5f86fb86f5a70e042dacce40a2ce2742250009ad
LorenzaVolponi <79290340+LorenzaVolponi@users.noreply.github.com>
2025-09-01 17:01:32 -0300

    test: cover routing to docs and 404

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,1 +1,15 @@
 import '@testing-library/jest-dom';
+
+// Polyfill matchMedia for components that rely on it
+if (!window.matchMedia) {
+  window.matchMedia = () => ({
+    matches: false,
+    media: '',
+    onchange: null,
+    addListener: () => {},
+    removeListener: () => {},
+    addEventListener: () => {},
+    removeEventListener: () => {},
+    dispatchEvent: () => false,
+  } as unknown as MediaQueryList);
+}
